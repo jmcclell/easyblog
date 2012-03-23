97dfb87afe3a910ff1780ed31d9ec11f8123b189
Jason <jason@jasonmcclellan.net>
2012-03-23 04:13:54 -0400

    As per zen-5, working toward moving all views to generic views if possible. This is work in progress and is a broken build.

--- easyblog/urls/posts.py
+++ easyblog/urls/posts.py
@@ -1,5 +1,5 @@
 """ URLs for Posts """
-from django.views.generic.dates import ArchiveIndexView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.conf.urls.defaults import url
 from django.conf.urls.defaults import patterns
 
@@ -11,18 +11,39 @@ archive_index_view = ArchiveIndexView.as_view(
     paginate_by=settings.POSTS_PER_PAGE,            
     allow_empty=True,
     queryset=Post.live.all(),
-    template_name='easyblog/posts/archive.html',
-    context_object_name='posts'
+    context_object_name='posts',
+    template_name='easyblog/posts/archive_index.html'
+)
+
+archive_year_view = YearArchiveView.as_view(
+    date_field='publish_date',
+    paginate_by=settings.POSTS_PER_PAGE,            
+    allow_empty=True,
+    queryset=Post.live.all(),
+    context_object_name='posts',
+    make_object_list=True,
+    template_name='easyblog/posts/archive_year.html'
+)
+
+archive_month_view = MonthArchiveView.as_view(
+    date_field='publish_date',
+    paginate_by=settings.POSTS_PER_PAGE,            
+    allow_empty=True,
+    queryset=Post.live.all(),
+    context_object_name='posts',
+    template_name='easyblog/posts/archive_month.html'                              
 )
 
 urlpatterns = patterns(
     'easyblog.views.posts',
-    url(r'^$', archive_index_view, name='easyblog_post_archive_index'),
+    url(r'^$', archive_index_view,
+        name='easyblog_post_archive_index'),
     url(r'^page/(?P<page>\d+)/$', archive_index_view,
         name='easyblog_post_archive_index_paginated'),
-    url(r'^(?P<year>\d{4})/$',
-        'posts_by_year', 
+    url(r'^(?P<year>\d{4})/$', archive_year_view,        
         name='easyblog_post_archive_year'),
+    url(r'^(?P<year>\d{4})/page/(?P<page>\d+)/$', archive_year_view,        
+        name='easyblog_post_archive_year_paginated'),    
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
         'posts_by_month', 
         name='easyblog_post_archive_month'),
