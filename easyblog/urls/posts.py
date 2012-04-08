c1d47f6ae87bed83947fe86eb6e4f93585b836ee
Jason <jason@jasonmcclellan.net>
2012-04-08 05:15:19 -0400

    Converted archive views to generic views for archive index, year archive, month archive, and day archive.

--- easyblog/urls/posts.py
+++ easyblog/urls/posts.py
@@ -1,59 +1,37 @@
-""" URLs for Posts """
-from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.conf.urls.defaults import url
 from django.conf.urls.defaults import patterns
 
-from easyblog import settings
-from easyblog.models import Post
+from easyblog.views.posts import archive_index_view, archive_month_view, archive_year_view, archive_day_view
 
-archive_index_view = ArchiveIndexView.as_view(
-    date_field='publish_date',
-    paginate_by=settings.POSTS_PER_PAGE,            
-    allow_empty=True,
-    queryset=Post.live.all(),
-    context_object_name='posts',
-    template_name='easyblog/posts/archive_index.html'
-)
-
-archive_year_view = YearArchiveView.as_view(
-    date_field='publish_date',
-    paginate_by=settings.POSTS_PER_PAGE,            
-    allow_empty=True,
-    queryset=Post.live.all(),
-    context_object_name='posts',
-    make_object_list=True,
-    template_name='easyblog/posts/archive_year.html'
-)
-
-archive_month_view = MonthArchiveView.as_view(
-    date_field='publish_date',
-    paginate_by=settings.POSTS_PER_PAGE,            
-    allow_empty=True,
-    queryset=Post.live.all(),
-    context_object_name='posts',
-    template_name='easyblog/posts/archive_month.html'                              
-)
-
+#TODO Can I reduce each of these into a single regexp for both paginated and non-paginated view?
 urlpatterns = patterns(
-    'easyblog.views.posts',
+    '',
     url(r'^$', archive_index_view,
         name='easyblog_post_archive_index'),
-    url(r'^page/(?P<page>\d+)/$', archive_index_view,
+    url(r'^archive/$', archive_index_view,
+        name='easyblog_post_archive_index'),
+    url(r'^archive/page/(?P<page>\d+)/$', archive_index_view,        
         name='easyblog_post_archive_index_paginated'),
-    url(r'^(?P<year>\d{4})/$', archive_year_view,        
+    url(r'^archive/(?P<year>\d{4})/$', archive_year_view,        
         name='easyblog_post_archive_year'),
-    url(r'^(?P<year>\d{4})/page/(?P<page>\d+)/$', archive_year_view,        
-        name='easyblog_post_archive_year_paginated'),    
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
-        'posts_by_month', 
+    url(r'^archive/(?P<year>\d{4})/page/(?P<page>\d+)/$', archive_year_view,        
+        name='easyblog_post_archive_year_paginated'),
+    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{2})/$', archive_month_view,    
         name='easyblog_post_archive_month'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        'posts_by_day', 
+    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{2})/page/(?P<page>\d+)/$', archive_month_view,    
+        name='easyblog_post_archive_month_paginated'),
+    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', archive_day_view,    
         name='easyblog_post_archive_day'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
-        'post_detail', 
-        name='easyblog_post_detail'),
-    url(r'^(?P<object_id>\d+)/$',
-        'post_byid',
-        name='easyblog_post_byid'),
+    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/page/(?P<page>\d+)/$', archive_day_view,    
+        name='easyblog_post_archive_day_paginated'),
 )
+
+
+"""                    
+url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+    'post_detail', 
+    name='easyblog_post_detail'),
+url(r'^(?P<object_id>\d+)/$',
+    'post_byid',
+    name='easyblog_post_byid'),   
+"""
--- easyblog/views/posts.py
+++ easyblog/views/posts.py
@@ -1,3 +1,72 @@
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, DateDetailView
 
+from easyblog import settings
+from easyblog.models import Post
 
+class PostArchiveIndexView(ArchiveIndexView):
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PostArchiveIndexView, self).get_context_data(**kwargs)
+        context.update({'url_extra_kwargs': self.kwargs})
+        return context
+
+class PostYearArchiveView(YearArchiveView):
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PostYearArchiveView, self).get_context_data(**kwargs)
+        context.update({'url_extra_kwargs': self.kwargs})
+        return context
+    
+class PostMonthArchiveView(MonthArchiveView):
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PostMonthArchiveView, self).get_context_data(**kwargs)
+        context.update({'url_extra_kwargs': self.kwargs})
+        return context
+    
+class PostDayArchiveView(DayArchiveView):
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PostDayArchiveView, self).get_context_data(**kwargs)
+        context.update({'url_extra_kwargs': self.kwargs})
+        return context
+    
+archive_index_view = PostArchiveIndexView.as_view(
+    date_field='publish_date',
+    paginate_by=settings.POSTS_PER_PAGE,            
+    allow_empty=True,
+    queryset=Post.live.all(),
+    context_object_name='posts',
+    template_name='easyblog/posts/archive_index.html'
+)
+
+archive_year_view = PostYearArchiveView.as_view(
+    date_field='publish_date',
+    paginate_by=settings.POSTS_PER_PAGE,            
+    allow_empty=True,
+    queryset=Post.live.all(),
+    context_object_name='posts',
+    make_object_list=True,
+    template_name='easyblog/posts/archive_year.html'
+)
+
+archive_month_view = PostMonthArchiveView.as_view(
+    date_field='publish_date',
+    paginate_by=settings.POSTS_PER_PAGE,            
+    allow_empty=True,
+    queryset=Post.live.all(),
+    context_object_name='posts',
+    template_name='easyblog/posts/archive_month.html',    
+    month_format='%m'                 
+)
+
+archive_day_view = PostDayArchiveView.as_view(
+    date_field='publish_date',
+    paginate_by=settings.POSTS_PER_PAGE,            
+    allow_empty=True,
+    queryset=Post.live.all(),
+    context_object_name='posts',
+    template_name='easyblog/posts/archive_day.html',    
+    month_format='%m'                 
+)
 
